549c345459379051180ae43779029bbe6374c928
debian <debian@vbox.42mulhouse.fr>
2025-10-07 21:24:46 +0200

    correc

--- ex03/ft_print_alphabet.ml
+++ ex03/ft_print_alphabet.ml
@@ -1,4 +1,10 @@
 let ft_print_alphabet () =
-        print_endline "abcdefghijklmnopqrstuvwxyz";;
+	let startpoint = ref (int_of_char 'a') in
+	let endpoint = ref (int_of_char 'z') in
+	while !startpoint <= !endpoint do
+		print_char (char_of_int !startpoint);
+		startpoint := !startpoint + 1
+	done;
+	print_char '\n';;
 
 ft_print_alphabet ();;
